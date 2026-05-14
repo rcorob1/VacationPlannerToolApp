2cc60385af459d4779a239ee08d42f56c219a37a
rcorob <rac0026@uah.edu>
2026-05-14 17:38:45 -0500

    B5 a-b display detailed view of excursion with title and date, and enter edit and delete excursion information

--- app/src/main/java/com/example/vacationplannertool/AddExcursionActivity.java
+++ app/src/main/java/com/example/vacationplannertool/AddExcursionActivity.java
@@ -1,9 +1,12 @@
 package com.example.vacationplannertool;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -14,10 +17,20 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.vacationplannertool.database.repository;
 import com.example.vacationplannertool.entity.Excursion;
+import com.example.vacationplannertool.entity.Vacation;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class AddExcursionActivity extends AppCompatActivity {
     private repository repo;
+    private Excursion newExc;
+    final Calendar calExc = Calendar.getInstance();
+    DatePickerDialog.OnDateSetListener excDateDPD;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,18 +56,79 @@ public class AddExcursionActivity extends AppCompatActivity {
             public void onClick(View view) {
                 repo = new repository(getApplication());
 
-                EditText excNameField = findViewById(R.id.addExcNameField);
-                String newExcName = excNameField.getText().toString();
+
                 int vacId = getIntent().getIntExtra("vacId", -1);
-                if(vacId >= 0) {
-                    Excursion newExc = new Excursion();
-                    newExc.setExcName(newExcName);
+
+                repo = new repository(getApplication());
+
+                Vacation assocVac = new Vacation();
+                for(Vacation vac : repo.getVacations()) {
+                    if(vacId == vac.getVacId()) {
+                        assocVac = vac;
+                    }
+                }
+
+                EditText excUpdTextName = findViewById(R.id.addExcNameField);
+                EditText excUpdDate = findViewById(R.id.addExcDateField);
+                CheckBox excNotiBox = findViewById(R.id.addExcCheckBox);
+
+                String excNewName = excUpdTextName.getText().toString();
+                String excNewDate = excUpdDate.getText().toString();
+
+                String format = "yyyy/MM/dd";
+                SimpleDateFormat sdf = new SimpleDateFormat(format, Locale.US);
+                Date newDate = new Date();
+                Date vacStart = new Date();
+                Date vacEnd = new Date();
+                try {
+                    newDate = sdf.parse(excNewDate);
+                    vacStart = sdf.parse(assocVac.getVacStartDate());
+                    vacEnd = sdf.parse(assocVac.getVacEndDate());
+                } catch (Exception e) { e.printStackTrace(); }
+
+                if(newDate.before(vacStart) || newDate.after(vacEnd)) {
+                    Toast.makeText(AddExcursionActivity.this, "Excursion could not be created", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    newExc = new Excursion();
+                    newExc.setExcName(excNewName);
+                    newExc.setExcDate(excNewDate);
                     newExc.setVacId(vacId);
                     newExc.setExcId(0);
                     repo.insert(newExc);
                     Toast.makeText(AddExcursionActivity.this, "Excursion created successfully", Toast.LENGTH_LONG).show();
                     finish();
                 }
+            }
+        });
+
+        excDateDPD = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker dp, int yr, int mo, int dy) {
+                calExc.set(Calendar.YEAR, yr);
+                calExc.set(Calendar.MONTH, mo);
+                calExc.set(Calendar.DAY_OF_MONTH, dy);
+
+                String format = "yyyy/MM/dd";
+                SimpleDateFormat sdf = new SimpleDateFormat(format, Locale.US);
+                TextView addExcDate = findViewById(R.id.addExcDateField);
+                addExcDate.setText(sdf.format(calExc.getTime()));
+
+            }
+        };
+
+        TextView addExcDate = findViewById(R.id.addExcDateField);
+        addExcDate.setKeyListener(null);
+        addExcDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Date date;
+                new DatePickerDialog(AddExcursionActivity.this, excDateDPD,
+                        calExc.get(Calendar.YEAR),
+                        calExc.get(Calendar.MONTH),
+                        calExc.get(Calendar.DAY_OF_MONTH)).show();
             }
         });
 
--- app/src/main/java/com/example/vacationplannertool/DetailedExcursion.java
+++ app/src/main/java/com/example/vacationplannertool/DetailedExcursion.java
@@ -1,8 +1,12 @@
 package com.example.vacationplannertool;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,13 +18,22 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.vacationplannertool.database.repository;
 import com.example.vacationplannertool.entity.Excursion;
+import com.example.vacationplannertool.entity.Vacation;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class DetailedExcursion extends AppCompatActivity {
     private repository repo;
     private int vacId;
     private int excId;
     private String excName;
+    private Excursion currentExc;
+    final Calendar calExc = Calendar.getInstance();
+    DatePickerDialog.OnDateSetListener excDateDPD;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,7 +56,9 @@ public class DetailedExcursion extends AppCompatActivity {
         excId = intent.getIntExtra("excId", -1);
         repo = new repository(getApplication());
         TextView excTextName = findViewById(R.id.excDetTextName);
-        Excursion currentExc = new Excursion();
+        EditText excUpdTextName = findViewById(R.id.updExcNameField);
+        EditText excUpdDate = findViewById(R.id.updExcDateField);
+        currentExc = new Excursion();
         for(Excursion exc : repo.getExcursions()){
             if(exc.getExcId() == excId) {
                 currentExc = exc;
@@ -51,13 +66,27 @@ public class DetailedExcursion extends AppCompatActivity {
         };
 
         String excName = currentExc.getExcName();
+        String excDateStr = currentExc.getExcDate();
         excTextName.setText(excName);
+        excUpdTextName.setText(excName);
+        excUpdDate.setText(excDateStr);
+
+        String format = "yyyy/MM/dd";
+        SimpleDateFormat sdf = new SimpleDateFormat(format, Locale.US);
+        try {
+            Date excDate = sdf.parse(excDateStr);
+            if(excDate != null) {
+                calExc.setTime(excDate);
+            }
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
 
         FloatingActionButton excDelBtn = findViewById(R.id.excDeleteBtn);
         excDelBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Excursion currentExc = new Excursion();
+                currentExc = new Excursion();
                 for(Excursion exc : repo.getExcursions()){
                     if(exc.getExcId() == excId) {
                         currentExc = exc;
@@ -83,6 +112,84 @@ public class DetailedExcursion extends AppCompatActivity {
             }
 
         });
+
+        FloatingActionButton saveBtn = findViewById(R.id.addExcSaveBtn);
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                repo = new repository(getApplication());
+
+                Vacation assocVac = new Vacation();
+                for(Vacation vac : repo.getVacations()) {
+                    if(vacId == vac.getVacId()) {
+                        assocVac = vac;
+                    }
+                }
+
+                EditText excUpdTextName = findViewById(R.id.updExcNameField);
+                EditText excUpdDate = findViewById(R.id.updExcDateField);
+                CheckBox excNotiBox = findViewById(R.id.updExcCheckBox);
+
+                String excNewName = excUpdTextName.getText().toString();
+                String excNewDate = excUpdDate.getText().toString();
+
+                String format = "yyyy/MM/dd";
+                SimpleDateFormat sdf = new SimpleDateFormat(format, Locale.US);
+                Date newDate = new Date();
+                Date vacStart = new Date();
+                Date vacEnd = new Date();
+                try {
+                    newDate = sdf.parse(excNewDate);
+                    vacStart = sdf.parse(assocVac.getVacStartDate());
+                    vacEnd = sdf.parse(assocVac.getVacEndDate());
+                } catch (Exception e) { e.printStackTrace(); }
+
+                if(newDate.before(vacStart) || newDate.after(vacEnd)) {
+                    Toast.makeText(DetailedExcursion.this, "Excursion could not be updated", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    currentExc.setExcName(excNewName);
+                    currentExc.setExcDate(excNewDate);
+                    repo.update(currentExc);
+                }
+
+
+
+            }
+
+        });
+
+
+        excDateDPD = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker dp, int yr, int mo, int dy) {
+                calExc.set(Calendar.YEAR, yr);
+                calExc.set(Calendar.MONTH, mo);
+                calExc.set(Calendar.DAY_OF_MONTH, dy);
+
+                String format = "yyyy/MM/dd";
+                SimpleDateFormat sdf = new SimpleDateFormat(format, Locale.US);
+                TextView addExcDate = findViewById(R.id.updExcDateField);
+                addExcDate.setText(sdf.format(calExc.getTime()));
+
+            }
+        };
+
+        TextView addExcDate = findViewById(R.id.updExcDateField);
+        addExcDate.setKeyListener(null);
+        addExcDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Date date;
+                new DatePickerDialog(DetailedExcursion.this, excDateDPD,
+                        calExc.get(Calendar.YEAR),
+                        calExc.get(Calendar.MONTH),
+                        calExc.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
     }
 
 
